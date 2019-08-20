c62d71f384fbdce7c30bfb1a1674e3179c909cbb
Justin Abrams <justin.abrams@hotmail.com>
2019-08-20 17:11:30 +0200

    Added modal looks bad though

--- first_challenge/challenge_1/store.js
+++ first_challenge/challenge_1/store.js
@@ -17,6 +17,62 @@ function select(element) {
     element.className += " selected";
 
     let circle = element.getElementsByClassName('inner');
+
+    elements = document.getElementsByClassName('colorName')
+    elements[0].textContent = "COLOR: " + circle[0].id
+    elements[1].textContent = circle[0].id
     
-    document.getElementById('colour').textContent = "COLOR: " + circle[0].id;
 }
+
+/**
+ *  Getting all the elements i need to add listeners to
+ */
+
+ let add_to_cart = document.getElementById('add_to_cart');
+ let minus = document.getElementById('minus');
+ let plus = document.getElementById('plus');
+ let agree = document.getElementsByClassName('closeBtn')[0];
+ let cancle = document.getElementsByClassName('closeBtn')[1];
+
+ /**
+  * elements of page
+  */
+
+  let modal = document.getElementById('popup');
+  let modalNumber = document.getElementById('count');
+  let totalNumber = document.getElementById('colourCount');
+
+
+  /**
+   * functions
+   */
+
+   add_to_cart.addEventListener('click', openModal);
+   cancle.addEventListener('click', closeModal);
+   agree.addEventListener('click', addToTotal);
+   agree.addEventListener('click', closeModal);
+   minus.addEventListener('click', minus_one);
+   plus.addEventListener('click', add_one);
+
+   function openModal() {
+       modal.style.display = 'block';
+   }
+   function closeModal() {
+       modal.style.display = 'none';
+       modalNumber.textContent = '0';
+   }
+
+   function add_one() {
+        modalNumber.textContent = parseInt(modalNumber.textContent) + 1;
+   }
+   function minus_one() {
+        modalNumber.textContent = parseInt(modalNumber.textContent) - 1;
+   }
+   function addToTotal() {
+        totalNumber.textContent = modalNumber.textContent;
+        add_to_cart.textContent = "Check Out";
+        modalNumber.textContent = '0';
+   }
+
+
+
