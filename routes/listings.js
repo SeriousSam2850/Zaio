bae94e680b7c1aacb1a90796b1be5159c3df7678
Justin Abrams <justin.abrams@hotmail.com>
2019-10-05 23:56:04 +0200

    Added Objects to the database

--- routes/listings.js
+++ routes/listings.js
@@ -1,19 +1,36 @@
 const express = require('express')
 const router = express.Router()
+const listing = require('../models/listing')
 
 //Gettings All
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    try {
+        const listings = await listing.find()
+        res.json(listings)
+    } catch (err) {
+        res.status(500).json({ message: err.message})
+    }
 })
 
 //Getting One
 router.get('/:id', (req, res) => {
-    
+    res.send(req.params.id)
 })
 
 //Creating One
-router.post('/', (req, res) => {
-    
+router.post('/', async (req, res) => {
+    const property = new listing({
+        name: req.body.name,
+        location: req.body.location,
+        price: req.body.price
+    })
+
+    try {
+        const newProperty = await property.save()
+        res.status(201).json(newProperty)
+    } catch (error) {
+        res.status(400).json({ message: error.message})
+    }
 })
 
 //Updating One
