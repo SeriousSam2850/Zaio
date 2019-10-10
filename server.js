ba2f574baa63ca9a13282a5b7c2b8770daef62e3
Justin Abrams <justin.abrams@hotmail.com>
2019-10-10 18:26:25 +0200

    Test

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ db.once('open', () => console.log('Connected to database'))
 
 app.use(express.json())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // Routers
 const propertiesRouter = require('./routes/properties.js' )
 const customersRouter = require('./routes/customers.js' )
