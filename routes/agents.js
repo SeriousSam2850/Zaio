362b656d40d77c3e27653a9fc28c5fa07cf0567e
Justin Abrams <justin.abrams@hotmail.com>
2019-10-10 17:34:49 +0200

    No dup emails

--- routes/agents.js
+++ routes/agents.js
@@ -6,33 +6,78 @@ const bcrypt = require('bcrypt')
 //Creating One
 router.post('/', async (req, res) => {
 
-    //Hashing password
-    bcrypt.hash(req.body.email, 10, (err, hash) => {
-        if (err) {
-            return res.status(500).json({
-                error: err
-            });
-        } else {
-            const agent = new Agent({
-                firstname: req.body.firstname,
-                lastname: req.body.lastname,
-                email: req.body.email,
-                password: hash
-            });
-            agent
-                .save()
-                .then(result => {
-                    console.log(result);
-                    res.status(201).json({ message: "User created" })
-                })
-                .catch(err => {
-                    console.log(err);
-                    res.status(500).json({
-                        error: err
-                    });
-                })
-        }
-    });
+    Agent.find({email: req.body.email})
+        .exec()
+        .then(agent => {
+            if (agent) {
+                return res.status(409).json({
+                    message: "Mail exsits"
+                });
+            } else {
+            //Hashing password
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const agent = new Agent({
+                            firstname: req.body.firstname,
+                            lastname: req.body.lastname,
+                            email: req.body.email,
+                            password: hash
+                        });
+                        agent
+                            .save()
+                            .then(result => {
+                                console.log(result);
+                                res.status(201).json({ message: "User created" })
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                });
+                            })
+                    }
+                });
+            }
+        });
 })
+
+//Deleting One
+router.delete('/:id', getAgent, async (req, res) => {
+    try {
+        await res.agent.remove()
+        res.json({ message: 'Deleted Agent'})
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+});
+
+//Getting All
+router.get('/', async (req, res) => {
+    try {
+        const agents = await Agent.find()
+        res.json(agents)
+    } catch (err) {
+        res.status(500).json({ message: err.message})
+    }
+});
+
+async function getAgent(req, res, next) {
+    let agent
+    try {
+        agent = await Agent.findById(req.params.id)
+        if (agent == null) {
+            return res.status(404).json({ message: 'Cannot find agent' })
+        }
+    } catch (error) {
+        console.log("Error finding agent")
+        return res.status(500).json({ message: error.message })
+    }
+    res.agent = agent
+    next()
+}
 
 module.exports = router
