1deb7ecdc3cfa803fa507c0121020c5aa2aad1c9
Justin Abrams <justin.abrams@hotmail.com>
2019-10-10 17:16:30 +0200

    Auth

--- routes/agents.js
+++ routes/agents.js
@@ -1,23 +1,38 @@
 const express = require('express')
 const router = express.Router()
 const Agent = require('../models/agent')
-
+const bcrypt = require('bcrypt')
 
 //Creating One
 router.post('/', async (req, res) => {
-    const agent = new Agent({
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        password: req.body.password
-    })
 
-    try {
-        const newAgent = await agent.save()
-        res.status(201).json(newAgent)
-    } catch (error) {
-        res.status(400).json({ message: error.message})
-    }
+    //Hashing password
+    bcrypt.hash(req.body.email, 10, (err, hash) => {
+        if (err) {
+            return res.status(500).json({
+                error: err
+            });
+        } else {
+            const agent = new Agent({
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                email: req.body.email,
+                password: hash
+            });
+            agent
+                .save()
+                .then(result => {
+                    console.log(result);
+                    res.status(201).json({ message: "User created" })
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json({
+                        error: err
+                    });
+                })
+        }
+    });
 })
 
 module.exports = router
