0feae571d8f84998392de7c3c90393f222bae733
FLips <justin.abrams@hotmail.com>
2019-10-11 15:45:40 +0200

    Requirements

--- routes/properties.js
+++ routes/properties.js
@@ -41,7 +41,7 @@ router.post('/', checkAuth, async (req, res) => {
         imageUrl: req.body.imageUrl,
         price: req.body.price,
         geo: req.body.geo,
-        agentID: req.userData._id
+        agentID: req.body.userData._id
     })
 
     try {
